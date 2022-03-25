d7347dd65fccc3810acdc59b9a1fec21fd8236b5
Andy <nunuzuhh@gmail.com>
2022-03-24 21:09:46 -0700

    added music section

--- js/main.js
+++ js/main.js
@@ -1,50 +1,49 @@
 // Trailer Modal
-var modal = document.getElementById('trailer-modal');
-var btn = document.getElementById('btn-modal');
-var span = document.getElementsByClassName("close")[0];
+var modal = document.getElementById("trailer-modal");
+var openBtn = document.getElementById("btn-modal");
+var closeBtn = document.getElementsByClassName("close")[0];
 
 // On button click, open the Modal
-btn.onclick = () => {
-    modal.style.display = 'block';
-}
+openBtn.onclick = () => {
+  modal.style.display = "block";
+};
 
 // On span (x button) click, close the Modal
-span.onclick = function () {
-    stopVideo();
-    modal.style.display = 'none';
-}
-
+closeBtn.onclick = function () {
+  stopVideo();
+  modal.style.display = "none";
+};
 
 // Youtube iframe API: https://developers.google.com/youtube/iframe_api_reference
-var tag = document.createElement('script');
+var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/iframe_api";
 
-var firstScriptTag = document.getElementsByTagName('script')[0];
+var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', { videoId: 'gf0ZlqM1yfQ' });
+  player = new YT.Player("player", { videoId: "gf0ZlqM1yfQ" });
 }
 
 function stopVideo() {
-    player.stopVideo();
+  player.stopVideo();
 }
 
 // Scroll To Top
-const scrollToTop = document.querySelector('.scroll-to-top');
-const heroSection = document.querySelector('.hero-section');
+const scrollToTop = document.querySelector(".scroll-to-top");
+const heroSection = document.querySelector(".hero-section");
 const options = {
-    root: null,
-    threshold: 0.3,
+  root: null,
+  threshold: 0.3,
 };
 
-const observer = new IntersectionObserver(function(entries,observer) {
-  entries.forEach(entry => {
-    if(entry.isIntersecting) {
-        scrollToTop.style.display = 'none';
+const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      scrollToTop.style.display = "none";
     } else {
-        scrollToTop.style.display = 'block';
+      scrollToTop.style.display = "block";
     }
   });
 }, options);
